bd1cfc4df72640946176816c62d5a78d593810f5
Krzysztof Płocharz <krzysztof@plocharz.info>
2016-02-11 21:46:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
-uint mask[4];
+uint mask[4][4];
 uchar current_layer;
 uchar frame = 0;
 uchar brightness = 0;
@@ -37,8 +37,9 @@ void setup() {
 
   current_layer = 0;
   for(uchar y = 0; y < 4; y++){
-    mask[y] = (1 << 16) - 1;
-  };
+  for(uchar b = 0; b < 4; b++){
+    mask[y][b] = (1 << 16) - 1;
+  }};
   
   TCCR1A = 0;
   TCCR1B = 0;
@@ -54,16 +55,6 @@ ISR(TIMER1_COMPA_vect)
 }
 int counter = 0;
 
-void couting(){
-  digitalWrite(STCP, LOW);
-  shiftOut(DS, SHCP, MSBFIRST, counter);
-  shiftOut(DS, SHCP, MSBFIRST, counter >> 8);
-  enable_layer(counter % 4);
-  digitalWrite(STCP, HIGH);
-  
-  counter++;
-}
-
 void disable_layers(){
   for(int i = LAYER_LOW; i < LAYER_HIGH; i++){
     digitalWrite(i, HIGH);
@@ -77,29 +68,48 @@ void enable_layer(int layer){
   }
 }
 
+
+uchar frame_to_bmask[15] = {
+  0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3
+};
 void update(){
-  uint layer = mask[current_layer];
-  if(brightness > frame){
+  
+  uint layer = mask[current_layer][frame_to_bmask[frame]];
     digitalWrite(STCP, LOW);
     shiftOut(DS, SHCP, MSBFIRST, layer);
     shiftOut(DS, SHCP, MSBFIRST, layer >> 8);
     enable_layer(current_layer);
     digitalWrite(STCP, HIGH);
-  }else{
-    disable_layers();
-    digitalWrite(STCP, LOW);
-    shiftOut(DS, SHCP, MSBFIRST, 0);
-    shiftOut(DS, SHCP, MSBFIRST, 0);
-    digitalWrite(STCP, HIGH);
-  }
   current_layer += 1;
   current_layer %= 4;
   frame++;
   frame %= 15;
 }
 
+inline void set(uchar x, uchar y, uchar z, uchar b){
+  uchar bit_to_set = y + (z << 2);
+  uint bit_mask = 1 << bit_to_set;
+  if(b > 15){
+    b = 15;
+  }
+  for (uchar bit_from_b = 0; bit_from_b < 4; bit_from_b++){
+    if(b & (1 << bit_from_b)){
+      mask[x][bit_from_b] |= bit_mask;
+    } else {
+      mask[x][bit_from_b] &= ~bit_mask;
+    }
+  }
+}
+
 void loop() {
   counter++;
+  counter %= 1024;
   brightness = abs(counter % 32 - 16);
-  delay(25);
+  
+  for(uchar x = 0; x < 4; x++){
+  for(uchar y = 0; y < 4; y++){
+  for(uchar z = 0; z < 4; z++){
+    set(x, y, z, brightness);
+  }}};
+  delay(10);
 }
