810beec200ed9f8d922db6274a3ad371a3476cd5
Krzysztof Płocharz <krzysztof@plocharz.info>
2016-02-14 00:57:04 +0100

    3 animations

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
 
-//Pin connected to ST_CP of 74HC595
-#define STCP 30
-//Pin connected to SH_CP of 74HC595
-#define SHCP 31
-////Pin connected to DS of 74HC595
-#define DS 29
-//#define OE 27
-//#define MR 30
 #define LAYER_LOW 25
 #define LAYER_HIGH 29
 
@@ -24,22 +16,16 @@ uchar brightness = 0;
 #define LATCH (1<<PB4)          //SS   (RCK)
 #define CLOCK (1<<PB7)          //SCK  (SCK)
 
+#define ALL (1 << 16) - 1
+
 void enable_spi(){
   SHIFT_REGISTER |= (DATA | LATCH | CLOCK);     //Set control pins as outputs
   SHIFT_PORT &= ~(DATA | LATCH | CLOCK);                //Set control pins low
   SPCR = (1<<SPE) | (1<<MSTR);
 }
 
-void enable_soft(){
-  pinMode(STCP, OUTPUT);
-  pinMode(SHCP, OUTPUT);
-  pinMode(DS, OUTPUT);
-  
-}
-
 void setup() {
   delay(3000);
-//  enable_soft();
   enable_spi();
   for (int layer = LAYER_LOW; layer < LAYER_HIGH; layer++){
     pinMode(layer, OUTPUT);
@@ -49,7 +35,7 @@ void setup() {
   current_layer = 0;
   for(uchar y = 0; y < 4; y++){
   for(uchar b = 0; b < 4; b++){
-    mask[y][b] = (1 << 16) - 1;
+    mask[y][b] = ALL;
   }};
   
   TCCR1A = 0;
@@ -104,10 +90,10 @@ void update(){
 inline void set(uchar x, uchar y, uchar z, int b){
   uchar bit_to_set = y + (z << 2);
   uint bit_mask = 1 << bit_to_set;
-  if(b > 15){
-    b = 15;
-  }
+  
+  if(b > 15) b = 15;
   if(b < 0) b = 0;
+  
   for (uchar bit_from_b = 0; bit_from_b < 4; bit_from_b++){
     if(b & (1 << bit_from_b)){
       mask[x][bit_from_b] |= bit_mask;
@@ -117,43 +103,89 @@ inline void set(uchar x, uchar y, uchar z, int b){
   }
 }
 
+void clear_to(int b){
+  if(b > 15) b = 15;
+  if(b < 0) b = 0;
+  for (uchar bit_from_b = 0; bit_from_b < 4; bit_from_b++){
+    if(b & (1 << bit_from_b)){
+      for(uchar y = 0; y < 4; y++)
+        mask[y][bit_from_b] = ALL;
+    } else {
+      for(uchar y = 0; y < 4; y++)
+        mask[y][bit_from_b] = 0;
+    }
+  }
+}
+
+
 void loop(){
-  
   ball();
-//  blink_();
+  blink_();
+  rain();
 }
 
 void ball(){
-  counter++;
-  counter %= 1024;
-  brightness = abs(counter % 32 - 16);
-  
-  float bx = (sin(float(counter) / 9) + 1) * 1.5;
-  float by = (sin(float(counter) / 5) + 1) * 1.5;
-  float bz = (cos(float(counter) / 7) + 1) * 1.5;
-//  float bx = 1;
-//  float by = 1;
-//  float bz = 1;
-  
-  for(char x = 0; x < 4; x++){
-  for(char y = 0; y < 4; y++){
-  for(char z = 0; z < 4; z++){
-    float b = abs(bx - x) + abs(by - y) + abs(bz - z);
-    set(x, y, z, int(20 - b * 8));
-  }}};
-  
-  delay(10);
+  counter = 0;
+  while(counter < 1000){
+    counter++;
+    
+    float bx = (sin(float(counter) / 18) + 1) * 1.5;
+    float by = (sin(float(counter) / 10) + 1) * 1.5;
+    float bz = (cos(float(counter) / 14) + 1) * 1.5;
+    
+    for(char x = 0; x < 4; x++){
+    for(char y = 0; y < 4; y++){
+    for(char z = 0; z < 4; z++){
+      float b = abs(bx - x) + abs(by - y) + abs(bz - z);
+      set(x, y, z, int(20 - b * 8));
+    }}};
+    
+    delay(10);
+  };
 }
 
 void blink_() {
-  counter++;
-  counter %= 1024;
-  brightness = abs(counter % 32 - 16);
-  
+  counter = 0;
+  while(counter < 1000){
+    counter++;
+    brightness = (sin((counter / 90.0)* PI) + 1) * 8;
+    clear_to(brightness);
+    delay(10);
+  }
+}
+
+void rain(){
+  uchar dropx[4] = {random(4), random(4), random(4), random(4)};
+  uchar dropy[4] = {random(4), random(4), random(4), random(4)};
+  float dropz[4] = {random(4), random(4), random(4), random(4)};
+  counter = 0;
+  clear_to(0);
+  while(counter < 1000){
+    counter++;
+    for (uchar drop = 0; drop < 4; drop++){
+      for (uchar z = 0; z < 4; z++){
+        float b = 16 - abs(dropz[drop] - z) * 12;
+        if(b > 15) b = 0;
+        set(z, dropx[drop], dropy[drop], b);
+      }
+      dropz[drop] += 0.05;
+      if (dropz[drop] > 5){
+        dropz[drop] = -1;
+        dropx[drop] = random(4);
+        dropy[drop] = random(4);
+      }
+    }
+    delay(10);
+  }
+}
+
+
+void test(){
+  clear_to(0);
   for(uchar x = 0; x < 4; x++){
-  for(uchar y = 0; y < 4; y++){
-  for(uchar z = 0; z < 4; z++){
-    set(x, y, z, brightness);
-  }}};
-  delay(30);
+    for(uchar y = 0; y < 4; y++){
+      set(3, x, y, 15);
+    }
+  }
+  while(true);
 }
